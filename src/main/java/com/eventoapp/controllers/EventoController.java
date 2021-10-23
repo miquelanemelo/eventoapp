baba9034edc6a4b685f35c4b0c7caacb4acaacda
Miquelane Melo <miquelanepmelo@gmail.com>
2021-10-23 15:31:41 -0300

    alterando index.html

--- src/main/java/com/eventoapp/controllers/EventoController.java
+++ src/main/java/com/eventoapp/controllers/EventoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.eventoapp.models.Evento;
 import com.eventoapp.repository.EventoRepository;
@@ -12,17 +13,30 @@ import com.eventoapp.repository.EventoRepository;
 public class EventoController {
 	@Autowired
 	private EventoRepository er;
-	
-	@RequestMapping(value ="/cadastrarEvento", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/cadastrarEvento", method = RequestMethod.GET)
 	public String form() {
-		return "evento/formEvento";	}
+		return "evento/formEvento";
+	}
+
+	@RequestMapping(value = "/cadastrarEvento", method = RequestMethod.POST)
+	public String form(Evento evento) {
+
+		er.save(evento);
+
+		return "redirect:/cadastrarEvento";
+	}
+
+	@RequestMapping("/eventos")
+	public ModelAndView listaEventos(){
+		ModelAndView mv = new ModelAndView("index");
+		Iterable<Evento> eventos = er.findAll();
+		mv.addObject("eventos", eventos);
 		
-		@RequestMapping(value ="/cadastrarEvento", method=RequestMethod.POST)
-		public String form(Evento evento) {
-			
-			er.save(evento);
-			
-			return "redirect:/cadastrarEvento";	
+		return mv;
+		
+		
+
 	}
 
 }
