d4107496077c5f7d338166a3fa2029bcaf8bece4
Miquelane Melo <miquelanepmelo@gmail.com>
2021-10-24 16:08:02 -0300

    corrigindo redirecionamento

--- src/main/java/com/eventoapp/controllers/EventoController.java
+++ src/main/java/com/eventoapp/controllers/EventoController.java
@@ -4,6 +4,8 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,15 +29,16 @@ public class EventoController {
 		return model;
 	}
 
-	@RequestMapping(value = "/cadastro", method = RequestMethod.GET)
-	public String form() {
+	@GetMapping("/cadastro")
+	public String form(Model model) {
+		model.addAttribute("evento", new Evento());
 		return "evento/formEvento";
 	}
 
 	@RequestMapping(value = "/cadastrar", method = RequestMethod.POST)
 	public String form(Evento evento) {
 		repository.save(evento);
-		return "redirect:/cadastrarEvento";
+		return "redirect:/cadastro";
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
